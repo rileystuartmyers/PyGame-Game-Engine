712dfb6325c3d8c324d33649490657f7fc918ab7
rileystuartmyers <rileystuartmyers@gmail.com>
2025-08-25 16:20:03 -0500

    First working version of dodge game

--- main.py
+++ main.py
@@ -56,27 +56,42 @@ background = RED
 caption = ""
 
 pygame.init()
-rand = random.randrange(1)
-print(rand)
+
 FPS = 60
 fpsClock = pygame.time.Clock()
 SCREEN = pygame.display.set_mode(size)
 running = True
 
-ball = pygame.image.load(r"game/icons/slate.png")
-ball = rescaleImage(ball, 0.2)
-rect = ball.get_rect()
+randMin = 1
+randMax = 8
 
-blah = pygame.image.load(r"game/icons/blah.png")
-blah = rescaleImage(ball, 0.2)
-rect2 = blah.get_rect()
+randSpeedMin = 3
+randSpeedMax = 12
 
-background = pygame.image.load(r"game/checkered_background.png")
-background = redimensionImage(background, width, height)
-
+playerPath = r"icons/mcguckin.png"
+player = pygame.image.load(playerPath)
+player = redimensionImage(player, 60, 60)
+playerRect = player.get_rect()
 speedUnit = 3
 speed = [speedUnit, speedUnit]
-speedBot = [3, 1]
+
+blobPath0 = r"icons/bball.png"
+blob0 = pygame.image.load(blobPath0)
+blob0 = redimensionImage(blob0, 50, 50)
+rect0 = blob0.get_rect()
+blob0speed = [3, 0]
+
+blobPath1 = r"icons/sball.png"
+blob1 = pygame.image.load(blobPath1)
+blob1 = redimensionImage(blob1, 80, 80)
+rect1 = blob1.get_rect()
+blob1speed = [3, 0]
+
+
+backgroundPath = r"icons/checkered_background.png"
+background = pygame.image.load(backgroundPath)
+background = redimensionImage(background, width, height)
+
 
 while running:
 
@@ -100,33 +115,38 @@ while running:
                 speed[1] = -speed[1]    
 
 
-    rect = rect.move(speed)
-    rect2 = rect2.move(speedBot)
+    playerRect = playerRect.move(speed)
+    rect0 = rect0.move(blob0speed)
+    rect1 = rect1.move(blob1speed)
 
-    if (rect.left < 0) or (rect.right > width):
+    if (playerRect.left < 2) or (playerRect.right > width - 2):
 
         speed[0] = -speed[0]
 
-    if (rect.top < 0) or (rect.bottom > height):
+    if (playerRect.top < 2) or (playerRect.bottom > height - 2):
 
         speed[1] = -speed[1]
 
+    if (rect0.left > width):
 
-    if (rect2.left < 0) or (rect2.right > width):
+        rect0.bottom = width / random.randrange(randMin, randMax)
+        blob0speed[0] = random.randrange(randSpeedMin ,randSpeedMax)
+        rect0.right = 0
 
-        speedBot[0] = -speedBot[0]
+    if (rect1.left > width):
 
-    if (rect2.top < 0) or (rect2.bottom > height):
-
-        speedBot[1] = -speedBot[1]
+        rect1.bottom = width / random.randrange(randMin, randMax)
+        blob1speed[0] = random.randrange(randSpeedMin ,randSpeedMax)
+        rect1.right = 0
     
-    pygame.draw.rect(SCREEN, RED, rect, 1)
-    pygame.draw.rect(SCREEN, RED, rect2, 1)
+    pygame.draw.rect(SCREEN, RED, playerRect, 1)
+    pygame.draw.rect(SCREEN, RED, rect0, 1)
+    pygame.draw.rect(SCREEN, RED, rect1, 1)
 
     SCREEN.blit(background, (0, 0))
-    SCREEN.blit(ball, rect)
-    SCREEN.blit(blah, rect2)
-        
+    SCREEN.blit(player, playerRect)
+    SCREEN.blit(blob0, rect0)
+    SCREEN.blit(blob1, rect1)
 
     pygame.display.set_caption(caption)
     pygame.display.update()
