cf298781c7032b948ab32ff15cab365f32926318
rileystuartmyers <rileystuartmyers@gmail.com>
2025-09-26 17:41:44 -0500

    added settings icon to main menu, left play button value blank

--- game_files/mainMenuClass.py
+++ game_files/mainMenuClass.py
@@ -1,5 +1,6 @@
 from settings import DEFAULT_TEXTURE, DEFAULT_MENUBACKGROUND
 from settings import WINDOWx, WINDOWy, WINDOWSIZE
+from settings import DEFAULT_PLAYBUTTON_IMG, DEFAULT_SETTINGSBUTTON_IMG
 from imageutils import redimensionImage
 from renderutils import getWindowSize
 from mapClass import *
@@ -12,17 +13,20 @@ class mainMenu (map):
 
         map.__init__(self, name = "menu", size = WINDOWSIZE)
     
+
         self.SCREEN = SCREEN
         self.isActive = True
 
-        self.newgame_icon = pygame.image.load().convert_alpha()
-        self.continuegame_icon = pygame.image.load().convert_alpha()
-        self.options_icon = pygame.image.load().convert_alpha()
-        
+        self.playbutton_icon = pygame.image.load(DEFAULT_PLAYBUTTON_IMG).convert_alpha()
+        self.playbutton_icon = redimensionImage(self.playbutton_icon, 180, 100)
+
+        self.settings_icon = pygame.image.load(DEFAULT_SETTINGSBUTTON_IMG).convert_alpha()
+        self.settings_icon = redimensionImage(self.settings_icon, 100, 100)
+
         self.setBackground(DEFAULT_MENUBACKGROUND)
 
     def render(self):
 
         self.renderMap(SCREEN = self.SCREEN)
-        pygame.draw.rect(self.SCREEN, (240, 240, 240), (0, 0, 100, 100))
-
+        
+        self.SCREEN.blit(self.settings_icon, pygame.rect.Rect(self.res_width - 150, self.res_height - 150, 100, 100))
